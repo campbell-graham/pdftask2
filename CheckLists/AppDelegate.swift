793ce39e05bb424e9c06eebd1bbd92e160b44856
Campbell Graham <campbell.graham@gridstone.com.au>
2018-02-05 15:20:23 +1100

    Can now add and remove rows

--- CheckLists/AppDelegate.swift
+++ CheckLists/AppDelegate.swift
@@ -17,7 +17,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         window = UIWindow(frame: UIScreen.main.bounds)
-        window?.rootViewController = CheckListViewController()
+        let myCheckListViewController = CheckListViewController()
+        let navController = UINavigationController(rootViewController: myCheckListViewController)
+        //navController.navigationBar.prefersLargeTitles = true
+        window?.rootViewController = navController
         window?.makeKeyAndVisible()
         return true
     }
--- CheckLists/CheckListViewController.swift
+++ CheckLists/CheckListViewController.swift
@@ -13,6 +13,7 @@ class CheckListViewController : UITableViewController {
     var items: [CheckListItem]
     
     init() {
+       
         items = [CheckListItem]()
         
         items.append(CheckListItem(text: "Some text 0", checked: false))
@@ -22,6 +23,13 @@ class CheckListViewController : UITableViewController {
         items.append(CheckListItem(text: "Some text 4", checked: false))
         
         super.init(nibName: nil, bundle: nil)
+        
+        let addListItemBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addItem))
+        self.navigationItem.rightBarButtonItem = addListItemBarButtonItem
+        
+        self.title = "Items"
+        
+        
         
         //hides the excess rows of the table view
         tableView.tableFooterView = UIView()
@@ -33,6 +41,11 @@ class CheckListViewController : UITableViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
     
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        self.navigationController?.navigationBar.prefersLargeTitles = true
+    }
+    
     
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
@@ -40,7 +53,7 @@ class CheckListViewController : UITableViewController {
     
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 5
+        return items.count
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
@@ -63,6 +76,12 @@ class CheckListViewController : UITableViewController {
         tableView.deselectRow(at: indexPath, animated: true)
     }
     
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        items.remove(at: indexPath.row)
+        let indexPaths = [indexPath]
+        tableView.deleteRows(at: indexPaths, with: .automatic)
+    }
+    
     func configureCheckmark(for cell: UITableViewCell,
                             with item: CheckListItem) {
         if item.checked {
@@ -77,7 +96,13 @@ class CheckListViewController : UITableViewController {
         label.text = item.text
     }
     
-    
+    @IBAction func addItem() {
+        let newRowIndex = items.count
+        items.append(CheckListItem(text: "sdofjsdnckds", checked: false))
+        let indexPath = IndexPath(row: newRowIndex, section: 0)
+        let indexPaths = [indexPath]
+        tableView.insertRows(at: indexPaths, with: .automatic)
+    }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
