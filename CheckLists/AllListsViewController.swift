5707f03aa49aa21e15827f9d18f3849fea204625
Campbell Graham <campbell.graham@gridstone.com.au>
2018-02-07 14:52:38 +1100

    Lists are now split up into two sections, finished and not finished

--- CheckLists/AllListsViewController.swift
+++ CheckLists/AllListsViewController.swift
@@ -11,10 +11,12 @@ import UIKit
 class AllListsViewController: UITableViewController, ListDetailViewControllerDelegate, CheckListViewControllerDelegate, UINavigationControllerDelegate {
     
     var lists = [Checklist]()
+    var finishedLists = [Checklist]()
+    var notFinishedLists = [Checklist]()
     var addChecklistBarButtonItem: UIBarButtonItem!
     
     init() {
-        super.init(nibName: nil, bundle: nil)
+         super.init(style: .grouped)
         self.title = "Checklists"
     }
     
@@ -29,41 +31,91 @@ class AllListsViewController: UITableViewController, ListDetailViewControllerDel
         addChecklistBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(openListDetailController))
         navigationItem.rightBarButtonItem = addChecklistBarButtonItem
         loadChecklists()
-        tableView.reloadData()
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
-
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return lists.count
+        switch section {
+        case 0 : return finishedLists.count
+        case 1 : return notFinishedLists.count
+        default : return 0
+        }
     }
-
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 2
+    }
+    
+    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        if (section == 0){
+            return "Finished"
+        }
+        if (section == 1){
+            return "Outstanding"
+        }
+        return ""
+    }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
         var cell = tableView.dequeueReusableCell(withIdentifier: "cell")
         if cell == nil {
             cell = UITableViewCell(style: .subtitle, reuseIdentifier: "cell")
         }
-        let list = lists[indexPath.row]
-        cell?.textLabel!.text = list.name
-        let incompleteItems = list.items.filter({!$0.checked}).count
-        if list.items.count == 0 {
-            cell?.detailTextLabel?.text = "No items"
-        } else if incompleteItems == 0 {
-            cell?.detailTextLabel?.text = "All done!"
-        } else {
+        
+        switch indexPath.section {
+        //finished
+        case 0 : do {
+            let list = finishedLists[indexPath.row]
+            cell?.textLabel!.text = list.name
+            if list.items.count == 0 {
+                cell?.detailTextLabel?.text = "No items"
+            } else {
+                cell?.detailTextLabel?.text = "All done!"
+            }
+            }
+        //not finished
+        case 1 : do {
+            let list = notFinishedLists[indexPath.row]
+            let incompleteItems = list.items.filter({!$0.checked}).count
+            cell?.textLabel!.text = list.name
             cell?.detailTextLabel?.text = incompleteItems > 1 ? String("\(incompleteItems) items remaining") : String("\(incompleteItems) item remaining")
+            }
+            
+        //default to finished
+        default : do {
+            let list = finishedLists[indexPath.row]
+            cell?.textLabel!.text = list.name
+            if list.items.count == 0 {
+                cell?.detailTextLabel?.text = "No items"
+            } else {
+                cell?.detailTextLabel?.text = "All done!"
+            }
+            }
         }
         return cell!
     }
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        UserDefaults.standard.set(indexPath.row, forKey: "ChecklistIndex")
-        let destination = CheckListViewController(checklist: lists[indexPath.row])
-        destination.delegate = self
-        self.navigationController?.pushViewController(destination, animated: true)
+        UserDefaults.standard.set(indexPath.row, forKey: "ChecklistIndexRow")
+        UserDefaults.standard.set(indexPath.section, forKey: "ChecklistIndexSection")
+        switch indexPath.section {
+        case 0: do {
+            let destination = CheckListViewController(checklist: finishedLists[indexPath.row])
+            destination.delegate = self
+            self.navigationController?.pushViewController(destination, animated: true)
+        }
+        case 1: do {
+            let destination = CheckListViewController(checklist: notFinishedLists[indexPath.row])
+            destination.delegate = self
+            self.navigationController?.pushViewController(destination, animated: true)
+            }
+        default:
+            print("well this is awkward....I couldn't find this list!")
+        }
     }
     
     override func tableView(_ tableView: UITableView, editActionsForRowAt: IndexPath) -> [UITableViewRowAction]? {
@@ -86,23 +138,39 @@ class AllListsViewController: UITableViewController, ListDetailViewControllerDel
     func listDetailViewController(_ controller: ListDetailViewController, didFinishAdding checklist: Checklist) {
         lists.append(checklist)
         saveChecklists()
-        tableView.reloadData()
     }
     
     func listDetailViewController(_ controller: ListDetailViewController, didFinishEditing checklist: Checklist) {
         saveChecklists()
+    }
+    
+    
+    override func viewDidAppear(_ animated: Bool) {
+        navigationController?.delegate = self
+        let indexRow = UserDefaults.standard.integer(forKey: "ChecklistIndexRow")
+        let indexSection = UserDefaults.standard.integer(forKey: "ChecklistIndexSection")
+        if indexRow != -1 && indexSection != -1 && lists.count > 0{
+            if indexSection == 0 {
+                let destination = CheckListViewController(checklist: finishedLists[indexRow])
+                navigationController?.pushViewController(destination, animated: false)
+            } else if indexSection == 1 {
+                let destination = CheckListViewController(checklist: notFinishedLists[indexRow])
+                navigationController?.pushViewController(destination, animated: false)
+            }
+            else {
+                print ("Well this is awkard... I couldn't find that section")
+            }
+        }
         tableView.reloadData()
     }
     
-   
-    override func viewDidAppear(_ animated: Bool) {
-        navigationController?.delegate = self
-        let index = UserDefaults.standard.integer(forKey: "ChecklistIndex")
-        if index != -1 && lists.count > 0{
-            let destination = CheckListViewController(checklist: lists[index])
-            navigationController?.pushViewController(destination, animated: false)
-        }
-        tableView.reloadData()
+    override func viewWillAppear(_ animated: Bool) {
+        //finish lists are where there are no items left
+        finishedLists = lists.filter({$0.items.count == 0})
+        //lists that are not finished contain everything else
+        notFinishedLists = lists.filter({!finishedLists.contains($0)})
+        print(finishedLists.count)
+        print(notFinishedLists.count)
     }
     
     @IBAction func openListDetailController() {
@@ -151,8 +219,9 @@ class AllListsViewController: UITableViewController, ListDetailViewControllerDel
     func navigationController(_ navigationController: UINavigationController, willShow viewController: UIViewController, animated: Bool) {
         //checks if the back button was pressed
         if viewController === self {
-            UserDefaults.standard.set(-1, forKey: "ChecklistIndex")
+            UserDefaults.standard.set(-1, forKey: "ChecklistIndexRow")
+            UserDefaults.standard.set(-1, forKey: "ChecklistIndexSection")
         }
     }
-
+    
 }
